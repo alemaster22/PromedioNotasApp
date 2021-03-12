307bc626e42a5ea865063a81fa056600276b6c58
alemaster22 <alemaster222@gmail.com>
2021-03-11 23:36:49 -0500

    Add project files.

---
+++ Alumno.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PromedioNotasApp
+{ 
+    public class Alumno
+    {
+        private string nombreAlumno;
+        private double nota1;
+        private double nota2;
+        private double nota3;
+      
+        public string GetNombreAlumno()
+        {
+            return nombreAlumno;
+        }
+
+        public void SetNombreAlumno(string nombreAlumno)
+        {
+            this.nombreAlumno = nombreAlumno;
+        }       
+        
+        public void SetNota1(double nota1)
+        {
+            if (nota1 >= 0 && nota1 <= 5) this.nota1 = nota1;
+            else this.nota1 = 0;
+        }
+                
+        public void SetNota2(double nota2)
+        {
+            if (nota2 >= 0 && nota2 <= 5) this.nota2 = nota2;
+            else this.nota2 = 0;
+        }
+
+        public void SetNota3(double nota3)
+        {
+            if (nota3 >= 0.0 && nota3 <= 5.0) this.nota3 = nota3;
+            else this.nota3 = 0;
+        }
+
+        public double PromedioNotasAlumno()
+        {
+            return (this.nota1 + this.nota2+this.nota3) / 3;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PromedioNotasApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nAlumnos=0;
+
+            double promedioAlumnos;
+
+
+            Console.WriteLine("Promedio Notas App \n El siguiente programa calcula el promedio de 3 notas dado un número n de alumnos y también devuelve el promedio del salón");
+
+            Console.WriteLine("Por favor digite un número de alumnos seguido de la tecla intro: ");
+
+            try
+            {
+                nAlumnos = int.Parse(Console.ReadLine());
+            }
+            catch (FormatException eF)
+            {
+                Console.WriteLine("Sólo se adminte un valor entero para el número de alumnos");
+                Environment.Exit(0);
+            }
+            catch (OverflowException oE)
+            {
+                Console.WriteLine("Se ha introducido un valor muy alto , intente nuevamente");
+                Environment.Exit(0);
+            }
+
+            List<double> listaPromedios = new List<double>();
+
+            if (nAlumnos != 0)
+            {
+
+                for (int i = 1; i <= nAlumnos; i++)
+                {
+                Alumno Alumno = new Alumno();
+
+                Console.WriteLine("Por favor introduza un nombre para el alumno: " + i);
+
+                Alumno.SetNombreAlumno(Console.ReadLine());
+
+                 Console.WriteLine("Por favor digite un valor de 0 a 5 para la nota1 del Alumno:" + Alumno.GetNombreAlumno());
+
+                 Alumno.SetNota1(ValidaNumero(Console.ReadLine()));
+
+                Console.WriteLine("Por favor digite un valor de 0 a 5 para la nota2 del Alumno:" + Alumno.GetNombreAlumno());
+
+                Alumno.SetNota2(ValidaNumero(Console.ReadLine()));
+
+                Console.WriteLine("Por favor digite un valor de 0 a 5 para la nota3 del Alumno:" + Alumno.GetNombreAlumno());
+
+                Alumno.SetNota3(ValidaNumero(Console.ReadLine()));
+
+                Console.WriteLine("El promedio del alumno: " + Alumno.GetNombreAlumno() + " es: " + Alumno.PromedioNotasAlumno());
+
+                listaPromedios.Add(Alumno.PromedioNotasAlumno());
+
+                }
+
+                double sumaPromedios=0;
+
+                foreach(var num in listaPromedios)
+                {
+                    sumaPromedios = num + sumaPromedios;
+                }
+
+                Console.WriteLine("El promedio del salón es: " + sumaPromedios / nAlumnos);
+
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Debe ingresar un valor mayor de 0 para el número de alumnos");
+            }
+        }
+        
+
+        public static double ValidaNumero(string numeroEntrada)
+        {
+            
+            try
+            {
+                var numSalida = double.Parse(numeroEntrada);
+
+                if(numSalida > 0 && numSalida <=5)
+                {
+                    return numSalida;
+                }
+                else
+                {
+                    Console.WriteLine("Sólo se adminte un valor númerico para las notas entre 0 y 5");
+                    Environment.Exit(0);
+                    return 0;
+                }
+
+                
+            }            
+            catch (FormatException eF)
+            {
+                Console.WriteLine("Sólo se adminte un valor númerico para las notas de los alumnos");
+                
+                Environment.Exit(0);
+                return 0;
+            }
+            catch (OverflowException oE)
+            {
+                Console.WriteLine("Se ha introducido un valor muy alto , intente nuevamente");
+                
+                Environment.Exit(0);
+                return 0;
+            }
+
+
+        }
+    }
+}
